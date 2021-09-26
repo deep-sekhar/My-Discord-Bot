fc8f9440ee8b984aa2ad7f35815d7f20dc8edfce
Deep <sekharsmemories@gmail.com>
2021-09-26 10:34:20 +0530

    added features

--- commands/botcom.js
+++ commands/botcom.js
@@ -1,26 +1,149 @@
+const fetch = require("node-fetch");
+
 module.exports =[{
     name: 'ping',
-    description: 'Ping!',
+    description: 'My 1st ever command',
     execute(msg, args) {
       msg.reply('pong');
       // msg.channel.send('pong');
     }
   },
   {
-    name: 'hemlo',
-    description: 'hii!',
+    name: 'pepk',
+    description: 'pepking emoji',
     execute(msg, args) {
-      msg.reply('hi!');
-      // msg.channel.send('pong');
+      msg.channel.send('<:PepeKingLove:834691690396385290>');
+    }
+  },
+  {
+    name: 'about',
+    description: 'about me',
+    execute(msg, args) {
+      msg.channel.send("",{files: ["./commands/images/dog.png"]});
+      msg.reply('I ..am a result of all 4 who came before me :-)');
     }
   },
   {
     name: 'spam',
-    description: 'spam!',
+    description: 'spam time!',
     execute(msg, args) {
       for(let i =0; i<10; i++)
-      // msg.reply('server spammed!');
-      msg.channel.send('server spammed!');
+      msg.channel.send(args);
     }
+  },
+  {
+    name: 'wao',
+    description: 'wao emoji',
+    execute(msg, args) {
+      msg.channel.send("",{files: ["./commands/images/1.png"]});
+    }
+  },
+  {
+    name: 'avengers assemble',
+    description: 'call everyone',
+    execute(msg, args) {
+      msg.channel.send(`<@854910698727407656>--<@760915500745359400>--<@787377550917238784>--<@784423230525014036>--<@778992537229983754>    assemble`);
+    }
+  },
+  {
+    name: 'dog',
+    description: 'dog pics!',
+   execute (msg, args) {
+    imm = async()=>{
+      try{
+        res = await fetch("https://dog.ceo/api/breeds/image/random");
+        console.info(res.status)
+        if(res.status>=200 || res.status<300)
+        {
+        json = await res.json();
+        linker = json.message;
+        // console.info(linker);
+        msg.channel.send("", {files: [linker]});
+        }
+        else throw new Error(res.status) 
+      }
+      catch(e){
+      msg.channel.send("error occured2 "+e);
+      }
+      }
+    imm();
+  }
+  },
+  {
+    name: 'cat',
+    description: 'cat pics!',
+   execute (msg, args) {
+    imm = async()=>{
+      try{
+        res = await fetch("https://api.thecatapi.com/v1/images/search");
+        console.info(res.status)
+        if(res.status>=200 || res.status<300)
+        {
+        json = await res.json();
+        linker = json[0].url;
+        // console.info(linker);
+        msg.channel.send("", {files: [linker]});
+        }
+        else throw new Error(res.status) 
+      }
+      catch(e){
+      msg.channel.send("error occured2 "+e);
+      }
+      }
+    imm();
+  }
+  },
+  {
+    name: 'dare',
+    description: 'want a dare?',
+   execute (msg, args) {
+    task = async()=>{
+      try{
+        res = await fetch("https://www.boredapi.com/api/activity");
+        console.info(res.status)
+        if(res.status>=200 || res.status<300)
+        {
+        json = await res.json();
+        job = json.activity;
+        msg.reply(job);
+        }
+        else throw new Error(res.status) 
+      }
+      catch(e){
+      msg.channel.send("error occured. DETAILS: "+e);
+      }
+      }
+    task();
+  }
+  },
+  {
+    name: 'joke',
+    description: 'a random joke!',
+   execute (msg, args) {
+    joke = async()=>{
+      try{
+        res = await fetch("https://official-joke-api.appspot.com/random_joke");
+        console.info(res.status)
+        if(res.status>=200 || res.status<300)
+        {
+        json = await res.json();
+        linker1 = json.setup;
+        linker2= json.punchline;
+        msg.channel.send(linker1);
+        setTimeout(() => {
+        msg.reply(linker2);
+        },6000 );
+        }
+        else {
+          console.log("RESPONSE STATUS:"+res.status)
+          throw new Error(res.status) 
+        }
+      }
+      catch(e){
+      msg.channel.send("error occured.\nDETAILS: "+e);
+      }
+      }
+    joke();
+  }
   }
   ];
--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
 
 // CONFIGURE ENV FILES FOR OFFLINE WORK
-require('dotenv').config({path:'./config.env'})
+require('dotenv').config({ path: './config.env' })
 
 // -------------------------------------------------------------------
 // DISCORD API 
@@ -27,48 +27,61 @@ const cron = require('cron');
 client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 
-  let scheduledMessage = new cron.CronJob('00 00 01 * * *', () => {
-    // This runs every day at 1AM, you can do anything you want
-    // Specifing your guild (server) and your channel
-       const guild = client.guilds.cache.get('856472795738800138');
-       const channel = guild.channels.cache.get('856472796187197482');
-       channel.send('Gd n8 boys!');
-       const guild2 = client.guilds.cache.get('760917131390091266');
-       const channel2 = guild2.channels.cache.get('789432643280044092');
-       channel2.send('Gd n8 boys!');
-      });
-          
-    // When you want to start it, use:
-    scheduledMessage.start()
+  //STATUS
+  client.user.setActivity('$$help', { type: 'PLAYING' });
 
-  let scheduledMessage2 = new cron.CronJob('00 30 08 * * *', () => {
+  let scheduledMessage = new cron.CronJob('00 30 08 * * *', () => {
     // This runs every day at 1AM, you can do anything you want
     // Specifing your guild (server) and your channel
-       const guild = client.guilds.cache.get('856472795738800138');
-       const channel = guild.channels.cache.get('856472796187197482');
-       channel.send('Gd morning boys!');
-       const guild2 = client.guilds.cache.get('760917131390091266');
-       const channel2 = guild2.channels.cache.get('789432643280044092');
-       channel2.send('Gd morning boys!');
-      });
-          
-    // When you want to start it, use:
-    scheduledMessage2.start()
-  });
+    const guild = client.guilds.cache.get('856472795738800138');
+    const channel = guild.channels.cache.get('856472796187197482');
+    channel.send('Gd morning boys!');
+    const guild2 = client.guilds.cache.get('760917131390091266');
+    const channel2 = guild2.channels.cache.get('789432643280044092');
+    channel2.send('Gd morning boys!');
+  }, undefined, true, "Asia/Kolkata");
+
+  let scheduledMessage2 = new cron.CronJob('00 25 00 * * *', () => {
+    // This runs every day at 1AM, you can do anything you want
+    // Specifing your guild (server) and your channel
+    const guild = client.guilds.cache.get('856472795738800138');
+    const channel = guild.channels.cache.get('856472796187197482');
+    channel.send('Gd n8 boys!');
+    const guild2 = client.guilds.cache.get('760917131390091266');
+    const channel2 = guild2.channels.cache.get('789432643280044092');
+    channel2.send('Gdn8 boys!');
+  }, undefined, true, "Asia/Kolkata");
+
+  let scheduledMessage3 = new cron.CronJob('00 09 20 * * *', () => {
+    const guild2 = client.guilds.cache.get('760917131390091266');
+    const channel2 = guild2.channels.cache.get('789432643280044092');
+    channel2.send('Game Time boys!');
+  }, undefined, true, "Asia/Kolkata");
+
+  // When you want to start it, use:
+  scheduledMessage2.start()
+  scheduledMessage.start()
+  scheduledMessage3.start()
+});
 
 // NORMAL COMMANDS 
 client.on('message', msg => {
   // check if the message is from our own bot and ignore it if it is
   if (msg.author.bot) return;
 
-  // if it is me 
+  if(msg.content == "gd n8") return(msg.channel.send('gd n8'));
+
+  // if bot is mentioned
   const mentionedMembersCollection = msg.mentions.members;
-  // console.info(mentionedMembersCollection)
-  if(mentionedMembersCollection.has('854910698727407656')) {
-    return(msg.channel.send('Who pong ?'));
-  }
+  // deep id - 854910698727407656
+  if(mentionedMembersCollection){
+  if (mentionedMembersCollection.has('878180887953551401')) {
+    msg.channel.send("",{files: ["./commands/images/dog.png"]});
+    return (msg.channel.send('Who pong ?'));
+  }}
 
   // message starts with the prefix we defined earlier and return if it doesn’t
+  if(msg.content === "clr") return(msg.channel.send(">clear 50"));
   if (!msg.content.startsWith(symb)) return;
 
   // DEBUG PURPOSE 
@@ -78,10 +91,22 @@ client.on('message', msg => {
   // console.info(`Called command: ${command}`);
 
   const command = msg.content.slice(2).toLowerCase().trim();
-  const args = ""
+  const args = msg.content.slice(2).toLowerCase().trim();
+
+  if(command === "help"){
+    let st ="COMMAND -- DESCRIPTION\n";
+    client.commands.forEach(ele=>{
+      st+= ele.name;
+      st+= " --> ";
+      st+= ele.description;
+      st+= "\n";
+    })
+    st+="CREATE -- WORK --INSPIRE!";
+    return (msg.channel.send(st));
+  }
 
   if (!client.commands.has(command)) {
-    return(msg.reply("Pardon Sir?"));
+    return (msg.reply("More commands will be added soon!"));
   }
   // mentionedMembersCollection.has(bot.user.id)
   console.info(client.user.id)
